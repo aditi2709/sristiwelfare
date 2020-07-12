51c27ea0485530f7e3ecc78332b3b2e86823fab1
@aditi <aditi.ghosh@geogo.in>
2020-07-13 02:05:19 +0530

    inspection result admin view modified

--- sristiwelfare2/wp-content/plugins/student-scholership/app/views/admin/inspection_results/add.php
+++ sristiwelfare2/wp-content/plugins/student-scholership/app/views/admin/inspection_results/add.php
@@ -4,6 +4,13 @@
 <?php echo $this->form->open_admin_table(); ?>
 <?php echo $this->form->input('student_id'); ?>
 <?php echo $this->form->input('status'); ?>
+<?php $options = array(
+  'Requested'=>'Requested',
+  'In-progress'=>'In-progress',
+  'Rejected'=>'Rejected',
+  'Verified'=>'Verified',
+  'Declined'=>'Declined');
+echo $this->form->select('status', array( 'label'=>'Status','options' => $options)); ?>
 <?php echo $this->form->input('date_of_activity',,array('type' => 'date')); ?>
 <?php echo $this->form->input('note'); ?>
 <?php echo $this->form->input('inspected_by'); ?>
